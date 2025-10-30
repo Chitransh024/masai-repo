9066fa8d3b3235e12f8f1433a68a8715dae58cb5
Chitransh024 <153779749+Chitransh024@users.noreply.github.com>
2025-10-30 15:49:15 +0530

    Add files via upload

---
+++ Basic Closure Example.js
@@ -0,0 +1,18 @@
+
+function outerFunction() {
+  let message = "Hello from the closure!";
+
+  
+  function innerFunction() {
+    console.log(message);
+  }
+
+  
+  return innerFunction;
+}
+
+
+const myClosure = outerFunction();
+
+
+myClosure(); 
