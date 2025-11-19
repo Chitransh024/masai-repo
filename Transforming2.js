9e0c85f586a075dc255bccd7d2f6bfe76b6f9aab
Chitransh024 <153779749+Chitransh024@users.noreply.github.com>
2025-11-19 20:38:16 +0530

    Add files via upload

---
+++ Transforming2.js
@@ -0,0 +1,6 @@
+function processStudents(students) {
+    return students
+        .filter(student => student.marks > 60)
+        .sort((a, b) => b.marks - a.marks)
+        .map(student => student.name);
+}
